c42c58a9b8932fdee650fa89c19f3f28e126c257
Nico <nicorabarisoa@gmail.com>
2024-07-24 15:32:02 +0200

    computer, humanchoice function added

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+let humanScore=0;
+let computerScore=0;
+
 function getComputerChoice(){
     let computerChoice=["Rock","Paper","Scissor"];
+
+    // genère un nombre aléatoire entre 0 et 2
     return(computerChoice[Math.floor(Math.random()*3)]);
 }
 
-console.log(getComputerChoice());
+function getHumanChoice(){
+    let HumanChoice = prompt("Type Rock, Paper or Scissor");
+    while (HumanChoice !== "Rock" && HumanChoice !== "Paper" && HumanChoice !== "Scissor" ) {
+        HumanChoice = prompt("Type Rock, Paper or Scissor only please");
+      }
+      console.log(HumanChoice);
+
+    
+}
+
+function playRound(humanChoice, computerChoice){
+    
+}
+
